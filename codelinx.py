26270f56072adb549e235df3b80b88d0e11063e3
CJTD13 <cjtdunleavy@Gmail.com>
2022-01-14 16:25:45 +0000

    Update codelinx.py
    mangling internal variable names and adding variable editors

--- codelinx.py
+++ codelinx.py
@@ -1,21 +1,28 @@
 class fragment(object):
     def __init__(self, string):
-        self.string = string
+        self.__string = string
+        
     def __add__(self, other):
         if type(other) is link:
-            return fragment(self.string+'\n'+other.string)
+            return fragment(self.__string+'\n'+other.get_string())
     def __call__(self):
-        return compile(self.string,'<string>','exec')
+        return compile(self.__string,'<string>','exec')
+    
+    def get_string(self):
+        return self.__string
+    
+    def set_string(self,newstring):
+        self.__string = newstring
         
 class link(object):
     def __init__(self,varlist):
-        self.vars = varlist
-        self.string = ''
+        self.__vars = varlist
+        self.__string = ''
 
 
     def __call__(self,**values):
         string = ''
-        for x in self.vars:
+        for x in self.__vars:
             if type(values[x]) is str:
                 string += x+ ' = '+'"'+str(values[x])+'"'
             else:
@@ -30,35 +37,64 @@ class link(object):
             return chain([self,other])
         elif type(other) is chain:
             return chain([self]+other.list)
+        
+    def get_string(self):
+        return self.__string
+    def get_vars(self):
+        return self.__vars
+    
+    def set_string(self,newstring):
+        self.__string = newstring
+        
+    def add_var(self,var):
+        pass
+    def remove_var(self,var):
+        pass
+    
 
 class chain(object):
     def __init__(self,linklist):
-        self.list = linklist
-        self.varlist = []
-        for i in self.list:
-            for j in i.varlist:
-                if j not in self.list:
-                    self.varlist.append(j)
+        self.__list = linklist
+        self.__varlist = []
+        for i in self.__list:
+            for j in i.get__vars():
+                if j not in self.__list:
+                    self.__varlist.append(j)
 
     def __call__(self, **values):
         init = ''
-        for x in self.varlist:
+        for x in self.__varlist:
             init += x +' = '+ str(values[x])
             init +='\n'
         selfstring = ''
-        for x in self.list:
-            self.string+=x.string
-            self.string +='\n'
+        for x in self.__list:
+            selfstring+=x.get_string()
+            selfstring +='\n'
         totstring = init+selfstring
         executor = fragment(totstring)
         return executor()
+    
+    def get_list(self):
+        return self.__list
+    def get_vars(self):
+        return self.__varlist
+    
+    def add_link(self,link):
+        pass
+    def add_var(self,var):
+        pass
+    
+    def remove_link(self,link):
+        pass
+    def remove_var(self,var):
+        pass
 
 class Bucket(object):
     def __init__(self):
-        self.funclist = {}
+        self.__funclist = {}
 
     def bindfunc(self,funcname,link):
-        self.funclist[funcname] = link
+        self.__funclist[funcname] = link
         setattr(self, funcname, link)
 
     def delfunc(self,func):
@@ -68,4 +104,7 @@ class Bucket(object):
         ans =[]
         for x in funcs:
             function = getattr(self,x)
+            
+    def get_funcs(self):
+        return self.__funclist
              
